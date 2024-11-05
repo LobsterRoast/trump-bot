8e6f6d4a35517cf9217ce6d7b6ec02d556a2ab63
lobsterroast <jekamminga@outlook.com>
2024-11-05 09:44:41 -0500

    Added some goofy comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Trump Bot by LobsterRoast
+// Do whatever you want with it
+
 use std::env;
 use std::string::String;
 
@@ -19,10 +22,23 @@ use ::serenity::all::Event;
 struct Data {}
 struct Handler;
 
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// Defines Keywords that Trump will respond to
+const KEYWORDS: [&str; 10] = [
+    "Clinton", "Obama", "Biden", "China", "Iran", "Russia", 
+    "Syria", "Bush", "Sanders", "illegal immigration"
+    ];
+
+// Implements the EventHandler trait in the Handler struct. This will handle things like detecting when a message is sent
 #[async_trait]
 impl EventHandler for Handler {
+    // message() runs whenever a message is sent
     async fn message(&self, ctx: OtherContext, msg: Message) {
         for keyword in KEYWORDS {
+            // we use to_ascii_lowercase() on both the sent message and the keywords to ensure that it's case insensitive
+            // Also checks msg.author.id to ensure that the bot doesn't respond to itself
             if msg.content.to_ascii_lowercase().contains(&keyword.to_ascii_lowercase()) && msg.author.id != 1300497365735833661 {
                 if let Err(why) = msg.channel_id.say(&ctx.http(), get_trump_quote(&keyword.to_string()).await.unwrap()).await {
                     println!("Error sending message: {why:?}");
@@ -31,23 +47,22 @@ impl EventHandler for Handler {
         }
     }
 }
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-const KEYWORDS: [&str; 10] = [
-    "Clinton", "Obama", "Biden", "China", "Iran", "Russia", 
-    "Syria", "Bush", "Sanders", "illegal immigration"
-];
 
+// Function to call from the TronaldDump.io API to get Trump tweets. Keyword is passed to the function
 async fn get_trump_quote(keyword: &str) -> Result<String, Error> {
+    // Gets a tweet with the given keyword in json format
     let url = format!("https://api.tronalddump.io/search/quote?query={}", keyword);
     let response = reqwest::get(&url).await?.text().await?;
     let json = json::parse(&response).expect("Failed to parse json.");
+    // Check how many tweets were provided in the json and picks a random one
     let count: usize = json["count"].as_usize().expect("Unable to parse json data to usize.");
     let rand = rand::thread_rng().gen_range(0..=count-1);
+    // Gets the actual content of the tweet from the json
     let quote: &str = json["_embedded"]["quotes"][rand]["value"].as_str().unwrap();
     Ok(quote.to_string())
 }
 
+// This function is just to make sure that the /bible command is case-insensitve. It sets the first alphabetic character to lowercase.
 fn format_book_name(mut book: String) -> String {
     book = book.to_ascii_lowercase();
     let mut book: Vec<char> = book.chars().collect();
@@ -59,15 +74,16 @@ fn format_book_name(mut book: String) -> String {
     }
     book.into_iter().collect()
 }
-// Posts a bible passage in chat
+// This is the code that runs when /bible is run
 #[poise::command(slash_command, prefix_command)]
 async fn bible(
     ctx: Context<'_>,
-    #[description = "Book of the Bible"] book: Option<String>,
-    #[description = "Chapter of the selected book"] chapter: Option<String>,
-    #[description = "Start Verse"] start: Option<u8>,
-    #[description = "End Verse"] end: Option<u8>
+    #[description = "Book of the Bible"] book: Option<String>, // Parameter to choose a book of the Bible
+    #[description = "Chapter of the selected book"] chapter: Option<String>, // Parameter to choose a chapter
+    #[description = "Start Verse"] start: Option<u8>, // Parameter to choose a starting verse
+    #[description = "End Verse"] end: Option<u8> // Parameter to choose an ending verse
 ) -> Result<(), Error> {
+    // Unwraps all the parameters so that they're usable
     let book = format_book_name(book.unwrap());
     let chapter = chapter.unwrap();
     let start = start.unwrap();
@@ -76,38 +92,46 @@ async fn bible(
     let file_path = format!("{}/json/verses-1769.json", 
                                     env::var("CARGO_MANIFEST_DIR")
                                     .expect("Couldn't find the root directory of the Rust project."));
+    // Parses the json file at file_path
     let parsed_json = json::parse(&std::fs::read_to_string(&file_path)
         .expect(&parsing_error_msg))
         .expect(parsing_error_msg);
     let unwrapped_book: &str = &book.to_string();
+    // Defines an empty vector of strings to which all requested verses can be pushed to
     let mut verses: Vec<String> = Vec::new();
     for verse in start..end+1 {
         verses.push(parsed_json[format!("{} {}:{}", book, chapter, verse)].as_str().unwrap().to_string());
     }
+    // Joins the verses vector into a single string
     let passage = verses.join(" ");
     let response = format!("I love the Bible. It's my favorite book. Let me read you my favorite passage in the WHOLE Bible:\n\n*\"{passage}\"*\n\nGod bless the USA.");
+    // Prints the response to the chat
     ctx.say(response).await?;
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
+    // Gets the bot token from the DISCORD_TOKEN environment variable
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN environment variable");
+    // Gives the bot the MESSAGE_CONTENT intent so that it can read messages
     let intents = serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
     let framework= poise::Framework::builder()
         .options(poise::FrameworkOptions {
+            // Sets the bible() method to be registered
             commands: vec![bible()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
+                // Registers all commands (in this case, just bible())
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data {})
             })
         })
         .build();
-
+    // Initializes the bot and attaches the an event handler to it
     let client = serenity::ClientBuilder::new(token, intents)
         .framework(framework)
         .event_handler(Handler)
